faf37aa547d0c00c16731d5062766dad10ab9359
Kismat Gurung <grgkismat74@gmail.com>
2022-02-08 22:01:21 +0545

    tab navigator updated in customer stack

--- SalonAppointmentBookingApp/src/navigation/CustomerStack.js
+++ SalonAppointmentBookingApp/src/navigation/CustomerStack.js
@@ -5,16 +5,36 @@ import SearchScreen from "../screens/CustomerScreens/Search/Search";
 import AppointmentScreen from "../screens/CustomerScreens/Appointments/Appointment";
 import NotificationScreen from "../screens/CustomerScreens/Notifications/Notification";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {View, Text} from "react-native";
 
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+const SearchStack = createNativeStackNavigator();
+
+const Detail = () => {
+    return (
+        <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+            <Text style={{color: "black"}}>Detail Screen</Text>
+        </View>
+    );
+}
+
+const SearchNavigator = () => {
+    return(
+        <SearchStack.Navigator screenOptions={{headerShown: false}}>
+            <SearchStack.Screen name="Search" component={SearchScreen}/>
+            <SearchStack.Screen name="Detail" component={Detail}/>
+        </SearchStack.Navigator>
+    );
+}
+
 
 const Main = () => {
     return (
         <Tab.Navigator>
             <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Search" component={SearchScreen} />
+            <Tab.Screen name="Discover" component={SearchNavigator} />
             <Tab.Screen name="CustomerAppointment" component={AppointmentScreen} />
             <Tab.Screen name="CustomerNotification" component={NotificationScreen} />
         </Tab.Navigator>
--- SalonAppointmentBookingApp/src/screens/CustomerScreens/Search/Search.js
+++ SalonAppointmentBookingApp/src/screens/CustomerScreens/Search/Search.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
 
-const SearchScreen = () => {
+const SearchScreen = ({ navigation }) => {
     return (
         <View style={{flex:1, justifyContent: "center", alignItems: "center"}}>
             <Text style={{color: "black"}}>Customer Search Screen</Text>
+            <Button title="Go to Detail" onPress={() => navigation.navigate("Detail")}/>
         </View>
     );
 }
